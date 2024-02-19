c5560c194d35c0e6a180e7aa53a678e457779db4
geekychristine <christine.martino@gmail.com>
2024-02-19 11:16:16 -0500

    feat: use button in app

--- apps/onion/src/app/app.tsx
+++ apps/onion/src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Button } from '@onion/ui';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="onion" />
+      <h1>Onion App Playground</h1>
+      <Button />
     </div>
   );
 }
