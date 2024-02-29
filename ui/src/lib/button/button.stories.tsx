3bee7fd421381f62ac19eff06ff802e312e3b3af
geekychristine <christine.martino@gmail.com>
2024-02-29 09:25:34 -0500

    feat(iu): add basic button theme

--- ui/src/lib/button/button.stories.tsx
+++ ui/src/lib/button/button.stories.tsx
@@ -7,18 +7,24 @@ import { expect } from '@storybook/jest';
 const meta: Meta<typeof Button> = {
   component: Button,
   title: 'Button',
+  tags: ['autodocs'],
+  args: {
+    title: 'Button',
+    size: 'md'
+  },
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg']
+    }
+  }
 };
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary = {
-  args: {},
-};
-
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/Welcome to Button!/gi)).toBeTruthy();
-  },
+export const Basic: Story = {};
+export const Fill: Story = {
+  args: {
+    title: 'Fill'
+  }
 };
--- ui/src/lib/button/button.tsx
+++ ui/src/lib/button/button.tsx
@@ -1,19 +1,47 @@
 import clsx from 'clsx';
 import styles from './button.module.css';
 
-/* eslint-disable-next-line */
 export interface ButtonProps {
   active: boolean;
   children: React.ReactNode;
+  loading: boolean;
+  icon: {
+    name: string;
+    position: 'before' | 'after';
+    size: 'sm' | 'md' | 'lg';
+  };
+  reference: React.RefObject<HTMLButtonElement>;
+  size: 'sm' | 'md' | 'lg';
+  variant: 'fill' | 'outline' | 'ghost';
   title: string;
   onClick: () => void;
 }
 
-export function Button({ children, title, active, ...rest }: ButtonProps) {
+/**
+ * @description A button component that can be used in various contexts.
+ */
+export function Button({
+  children,
+  title,
+  size = 'md',
+  active = false,
+  loading = false,
+  reference,
+  icon,
+  ...rest
+}: ButtonProps) {
+  const common =
+    'border-2 border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 dark:focus:ring-slate-500';
+  const theme =
+    'bg-slate-600 text-white hover:bg-slate-700 dark:bg-white dark:text-slate-800 dark:hover:bg-slate-800';
+  const sizes = { sm: `p-2`, md: 'p-4', lg: 'p-6' };
   return (
-    <button className={clsx(styles['button'], active)} {...rest}>
-      <span className="button-label">{title}</span>
-      {children}
+    <button
+      className={clsx(styles['button'], common, theme, sizes[size], active)}
+      ref={reference}
+      {...rest}
+    >
+      {children ? children : <span className="button-label">{title}</span>}
     </button>
   );
 }
